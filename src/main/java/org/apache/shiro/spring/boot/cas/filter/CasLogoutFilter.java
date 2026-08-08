3bcb034bee45a1cc19b50cddf7efae1e0aadea6f
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:54:13 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.2.x branch

--- src/main/java/org/apache/shiro/spring/boot/cas/filter/CasLogoutFilter.java
+++ src/main/java/org/apache/shiro/spring/boot/cas/filter/CasLogoutFilter.java
@@ -5,7 +5,7 @@ import org.apache.shiro.biz.web.filter.authc.AbstractLogoutFilter;
 
 /**
  * 实现Shiro登录注销与Cas单点登录注销的整合
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class CasLogoutFilter extends AbstractLogoutFilter {
 	
