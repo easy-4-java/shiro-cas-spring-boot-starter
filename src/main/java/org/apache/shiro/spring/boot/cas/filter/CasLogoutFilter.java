fbbd22e52efeaa2d5901beb0a45831743f9cd14e
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:08:26 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/cas/filter/CasLogoutFilter.java
+++ src/main/java/org/apache/shiro/spring/boot/cas/filter/CasLogoutFilter.java
@@ -5,7 +5,7 @@ import org.apache.shiro.biz.web.filter.authc.AbstractLogoutFilter;
 
 /**
  * 实现Shiro登录注销与Cas单点登录注销的整合
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class CasLogoutFilter extends AbstractLogoutFilter {
 	
--- src/main/java/org/apache/shiro/spring/boot/cas/realm/CasAssertionSimpleAuthorizingRealm.java
+++ src/main/java/org/apache/shiro/spring/boot/cas/realm/CasAssertionSimpleAuthorizingRealm.java
@@ -46,7 +46,7 @@ import org.jasig.cas.client.validation.TicketValidator;
 
 /**
  * Cas Stateless AuthorizingRealm
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @Slf4j
 public class CasAssertionSimpleAuthorizingRealm extends AbstractAuthorizingRealm {
